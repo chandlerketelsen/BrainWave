8d8e4a905ef890782872c8e677284d2c9f7c0a64
prakcoin <prakhar2@gmail.com>
2025-07-11 17:08:05 -0500

    Added initial training script and utils

---
+++ train.py
@@ -0,0 +1,43 @@
+import torch
+from torch_geometric.data import DataLoader
+from model.GNN import TemporalGNN
+from utils.train_utils import train_loop, val_loop, save_checkpoint
+from utils.geometric_graphs import build_graph_dataset
+
+batch_size = 32
+epochs = 30
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+distance_threshold = 75
+train_folder = ""
+val_folder = ""
+sequence_length = 12
+
+train_data_list = build_graph_dataset(train_folder)
+train_loader = DataLoader(train_data_list, batch_size=batch_size, shuffle=True)
+
+val_data_list = build_graph_dataset(train_folder)
+val_loader = DataLoader(val_data_list, batch_size=batch_size, shuffle=True)
+
+model = TemporalGNN(node_features=1, periods=sequence_length, batch_size=batch_size).to(device)
+learning_rate = 0.01
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.01, weight_decay=0.01, amsgrad=True)
+criterion = torch.nn.BCEWithLogitsLoss()
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=epochs)
+
+train_losses = []
+val_losses = []
+
+model.train()
+
+for epoch in range(epochs):
+    print(f"Epoch {epoch+1}")
+    train_loss = train_loop(train_loader, model, criterion, optimizer, device)
+    val_loss = val_loop(val_loader, model, criterion, device)
+
+    scheduler.step()
+    train_losses.append(train_loss)
+    val_losses.append(val_loss)
+
+    print(f"Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f}")
+    save_checkpoint(model, optimizer, scheduler, epoch, train_losses, val_losses)
---
+++ utils/train_utils.py
@@ -0,0 +1,76 @@
+import torch
+from tqdm import tqdm
+
+def save_checkpoint(model, optimizer, scheduler, epoch, train_losses, val_losses):
+  checkpoint = {
+          'epoch': epoch,
+          'model_state_dict': model.state_dict(),
+          'optimizer_state_dict': optimizer.state_dict(),
+          'scheduler_state_dict': scheduler.state_dict(),
+          'train_losses': train_losses,
+          'val_losses': val_losses,
+      }
+  torch.save(checkpoint, f'/checkpoints/tfpp_checkpoint_{epoch}.pt')
+  torch.save(model.state_dict(), f'/models/tfpp_model_{epoch}.pt')
+
+def load_checkpoint(filepath, model, optimizer, scheduler):
+    checkpoint = torch.load(filepath, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    start_epoch = checkpoint['epoch']
+    train_losses = checkpoint['train_losses']
+    val_losses = checkpoint['val_losses']
+    return model, optimizer, scheduler, start_epoch, train_losses, val_losses
+
+def train_loop(train_loader, model, criterion, optimizer, device, autoclipper=None, verbose=True):
+    model.train()
+    total_loss = 0.0
+    num_batches = len(train_loader)
+
+    for batch_idx, batch in enumerate(tqdm(train_loader)):
+        batch = batch.to(device)
+
+        x = batch.x
+        edge_index = batch.edge_index
+        y = batch.y.float()
+
+        optimizer.zero_grad()
+        out = model(x, edge_index)
+        loss = criterion(out, y)
+
+        loss.backward()
+        if autoclipper:
+            autoclipper(model)
+        optimizer.step()
+
+        total_loss += loss.item()
+
+        if (batch_idx + 1) % 100 == 0 and verbose:
+            print(f"Batch {batch_idx+1}, Loss: {total_loss / (batch_idx + 1):.6f}")
+
+    return total_loss / num_batches
+
+def val_loop(val_loader, model, criterion, device, verbose=False):
+    model.eval()
+    total_loss = 0.0
+    num_batches = len(val_loader)
+
+    with torch.no_grad():
+        for batch_idx, batch in enumerate(val_loader):
+            batch = batch.to(device)
+
+            x = batch.x
+            edge_index = batch.edge_index
+            y = batch.y.float()
+
+            out = model(x, edge_index)
+            loss = criterion(out, y)
+            total_loss += loss.item()
+
+            if verbose and (batch_idx + 1) % 50 == 0:
+                print(f"Val batch {batch_idx + 1}")
+                print(f"Predictions: {torch.sigmoid(out[:3])}")
+                print(f"Targets:     {y[:3]}")
+
+    return total_loss / num_batches
