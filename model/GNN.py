6ad231e2fd8f4de919dbf629d922f8c9201c59f6
prakcoin <prakhar2@gmail.com>
2025-07-11 19:37:25 -0500

    Added an additional A3TGCN2 layer to model

--- model/GNN.py
+++ model/GNN.py
@@ -1,22 +1,18 @@
 import torch
-from torch import Tensor
-from torch_geometric.nn import GCNConv
 from torch_geometric_temporal.nn.recurrent import A3TGCN2
-from torch_geometric.datasets import Planetoid
 import torch.nn.functional as F
 
 class TemporalGNN(torch.nn.Module):
-    def __init__(self, node_features, periods, batch_size):
-        super(TemporalGNN, self).__init__()
-        self.tgnn = A3TGCN2(in_channels=node_features,  out_channels=32, periods=periods,batch_size=batch_size) # node_features=2, periods=12
-        self.linear = torch.nn.Linear(32, periods)
+    def __init__(self, node_features, periods=16, batch_size=32):
+        super().__init__()
+        self.a3tgcn1 = A3TGCN2(in_channels=node_features, out_channels=64, periods=periods, batch_size=batch_size)
+        self.a3tgcn2 = A3TGCN2(in_channels=64, out_channels=64, periods=periods, batch_size=batch_size)
+        self.linear = torch.nn.Linear(64, 1)
 
     def forward(self, x, edge_index):
-        """
-        x = Node features for T time steps
-        edge_index = Graph edge indices
-        """
-        h = self.tgnn(x, edge_index) # x [b, 207, 2, 12]  returns h [b, 207, 12]
-        h = F.relu(h) 
-        h = self.linear(h)
+        h = self.a3tgcn1(x, edge_index)
+        h = F.relu(h)
+        h = self.a3tgcn2(h, edge_index)
+        h = F.relu(h)
+        h = self.linear(h).squeeze(-1)
         return h
